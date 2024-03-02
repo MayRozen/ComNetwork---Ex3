a1e02aad69f15c7d780e00a6c88d256651f5dde4
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-03-02 23:50:59 +0200

    Update RUDP_Sender.c

--- RUDP_Sender.c
+++ RUDP_Sender.c
@@ -8,16 +8,30 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#include <RUDP_API.h>
+
 #define SERVER_IP_ADDRESS "127.0.0.1"
 #define SERVER_PORT 5060
 
-// A struct that represents RUDP Socket
-typedef struct _rudp_socket {
-    int socket_fd; // UDP socket file descriptor
-    bool isServer; // True if the RUDP socket acts like a server, false for client.
-    bool isConnected; // True if there is an active connection, false otherwise.
-    struct sockaddr_in dest_addr; // Destination address. Client fills it when it connects via rudp_connect(), server fills it when it accepts a connection via rudp_accept().
-} RUDP_Socket;
+char *util_generate_random_data(unsigned int size) {
+    char *buffer;
+    // Argument check.
+    if (size == 0){
+        return NULL;
+    }
+    buffer = (char *)calloc(size, sizeof(char));
+    // Error checking.
+    if (buffer == NULL){
+        return NULL;
+    }
+    // Randomize the seed of the random number generator.
+    srand(time(NULL));
+    for (unsigned int i = 0; i < size; i++){
+        *(buffer + i) = ((unsigned int)rand() % 256);
+    }
+    return buffer;
+}
+
 
 /*RUDP_Socket* rudp_socket(bool isServer, unsigned short int listen_port); 
 int rudp_connect(RUDP_Socket *sockfd, const char *dest_ip, unsigned short int dest_port);
@@ -26,14 +40,17 @@ int rudp_close(RUDP_Socket *sockfd); */
 
 int main()
 {
+    printf("start of the RUDP_Sender\n");
     // Create socket
-	RUDP_Socket* rudp_socket(bool isServer, unsigned short int listen_port); 
+	int rudp_socket = RUDP_Socket* rudp_socket(bool isServer, unsigned short int listen_port);
+    unsigned int size2 = 2*1024*1024;
+    char* random_data = util_generate_random_data(size2); //Our file  
 
 	// Setup the server address structure.
 	// Port and IP should be filled in network byte order
     struct sockaddr_in RUDPreceiverAddress;
 	memset(&RUDPreceiverAddress, 0, sizeof(RUDPreceiverAddress));
-	RUDPreceiverAddress.sin_family = AF_INET;
+	RUDPreceiverAddress.sin_family = sockfd;
 	RUDPreceiverAddress.sin_port = htons(SERVER_PORT);
 	int rudp_connect = rudp_connect(RUDP_Socket *sockfd, const char *dest_ip, unsigned short int dest_port);
 	if (rudp_connect == 0){
@@ -44,18 +61,29 @@ int main()
         printf("rudp_connect success!");
     }
 
-	//send the message
-	int rudp_send = rudp_send(RUDP_Socket *sockfd, void *buffer, unsigned int buffer_size);
-    if(!isConnected){
-        perror("The socket disconnected");
-        return -1;
-    }
-    else if(udp_connect == 0){
-        printf("The packet disconnected");
-        return 0;
-    }
-    else{
-        return rudp_send;
+	while(1){
+        //send the message
+        int rudp_send = rudp_send(RUDP_Socket *sockfd, void *buffer, unsigned int buffer_size);
+        if(!isConnected){
+            perror("The socket disconnected");
+            return -1;
+        }
+        else if(rudp_connect == 0){
+            printf("The packet disconnected");
+            return 0;
+        }
+        else{
+            return rudp_send;
+        }
+
+        printf("Send the file again? y/n\n");
+        char c = getchar();
+        if(c == 'n'){
+            break;
+        }
+        else{
+            continue;
+        }
     }
 
 	struct sockaddr_in fromAddress;
@@ -65,10 +93,11 @@ int main()
 	memset((char *)&fromAddress, 0, sizeof(fromAddress));
 
 	// try to receive some data, this is a blocking call
-	if (recvfrom(s, bufferReply, sizeof(bufferReply) -1, 0, (struct sockaddr *) &fromAddress, &fromAddressSize) == -1)
-	{
+	if (recvfrom(s, bufferReply, sizeof(bufferReply) -1, 0, (struct sockaddr *) &fromAddress, &fromAddressSize) == -1){
 		printf("recvfrom() failed");
-		return -1;
+        free(random_data);
+        rudp_close(rudp_socket);
+        return -1;
 	}
     int rudp_disconnect(RUDP_Socket *sockfd);
     if(rudp_disconnect == 1){
@@ -78,7 +107,6 @@ int main()
         printf("The socket is already disconnected");
     }
 
-	udp_close(rudp_socket);
-
+	rudp_close(rudp_socket);
     return 0;
 }
