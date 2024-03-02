ad35f70c1e18e3da2cad74da31d2533134a77a5d
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-03-02 23:51:25 +0200

    Update RUDP_Receiver.c

--- RUDP_Receiver.c
+++ RUDP_Receiver.c
@@ -8,45 +8,39 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#include <RUDP_API.h>
+
 #define SERVER_IP_ADDRESS "127.0.0.1"
 #define SERVER_PORT 5060
 
 
 int main()
 {
-	int s = -1;
-	char buffer[80] = { '\0' };
-	char message[] = "Hello, from the Server\n";
-	int messageLen = strlen(message) + 1;
-
-	// Create socket
-	if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) // In Windows -1 is SOCKET_ERROR
-	{
-		printf("Could not create socket");
-			return -1;
-	}
+	signal(SIGPIPE, SIG_IGN); // prevent crash on closing socket
+    int listeningSocket = -1; // Open the listening (Receiver) socket
+    char receive_buff[256], send_buff[256];
+    struct sockaddr_in sender;
+    int sender_size;
 
 	// setup Server address structure
-	struct sockaddr_in serverAddress;
-	memset((char *)&serverAddress, 0, sizeof(serverAddress));
-	serverAddress.sin_family = AF_INET;
-	serverAddress.sin_port = htons(SERVER_PORT);
-	inet_pton(AF_INET, (const char*)SERVER_IP_ADDRESS, &(serverAddress.sin_addr));
+	struct sockaddr_in RUDPreceiverAddress;
+	memset((char *)&RUDPreceiverAddress, 0, sizeof(RUDPreceiverAddress));
+	RUDPreceiverAddress.sin_family = sockfd;
+	RUDPreceiverAddress.sin_port = htons(SERVER_PORT);
+	rudp_connect(RUDP_Socket *sockfd, const char *dest_ip, unsigned short int dest_port);
 
 	//Bind
-	if (bind(s, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1)
-	{
+	if (bind(listeningSocket, (struct sockaddr *)&RUDPreceiverAddress, sizeof(RUDPreceiverAddress)) == -1){
 		printf("bind() failed");
-		// TODO: cleanup the socket;
 		return -1;
 	}
-	printf("After bind(). Waiting for clients");
+	printf("After bind(). Waiting for Sender");
 
 	// setup Client address structure
-	struct sockaddr_in clientAddress;
-	socklen_t clientAddressLen = sizeof(clientAddress);
+	struct sockaddr_in RUDPsenderAddress;
+	socklen_t RUDPsenderAddressLen = sizeof(RUDPsenderAddress);
 
-	memset((char *)&clientAddress, 0, sizeof(clientAddress));
+	memset((char *)&RUDPsenderAddress, 0, sizeof(RUDPsenderAddress));
 
 	//keep listening for data
 	while (1)
@@ -54,8 +48,8 @@ int main()
 		fflush(stdout);
 
 		// zero client address 
-		memset((char *)&clientAddress, 0, sizeof(clientAddress));
-		clientAddressLen = sizeof(clientAddress);
+		memset((char *)&RUDPsenderAddress, 0, sizeof(RUDPsenderAddress));
+		RUDPsenderAddressLen = sizeof(RUDPsenderAddress);
 
 		//clear the buffer by filling null, it might have previously received data
 		memset(buffer, '\0', sizeof (buffer));
@@ -63,29 +57,27 @@ int main()
 		int recv_len = -1;
 
 		//try to receive some data, this is a blocking call
-		if ((recv_len = recvfrom(s, buffer, sizeof(buffer) -1, 0, (struct sockaddr *) &clientAddress, &clientAddressLen)) == -1)
+		if ((recv_len = recvfrom(s, buffer, sizeof(buffer) -1, 0, (struct sockaddr *) &RUDPsenderAddress, &RUDPsenderAddressLen)) == -1)
 		{
 			printf("recvfrom() failed");
 			break;
 		}
 
 		char clientIPAddrReadable[32] = { '\0' };
-		inet_ntop(AF_INET, &clientAddress.sin_addr, clientIPAddrReadable, sizeof(clientIPAddrReadable));
+		inet_ntop(AF_INET, &RUDPsenderAddress.sin_addr, clientIPAddrReadable, sizeof(clientIPAddrReadable));
 
 		//print details of the client/peer and the data received
-		printf("Received packet from %s:%d\n", clientIPAddrReadable, ntohs(clientAddress.sin_port));
+		printf("Received packet from %s:%d\n", clientIPAddrReadable, ntohs(RUDPsenderAddress.sin_port));
 		printf("Data is: %s\n", buffer);
 
 		//now reply to the Client
-		if (sendto(s, message, messageLen, 0, (struct sockaddr*) &clientAddress, clientAddressLen) == -1)
+		if (sendto(s, message, messageLen, 0, (struct sockaddr*) &RUDPsenderAddress, RUDPsenderAddressLen) == -1)
 		{
 			printf("sendto() failed");
 			break;
 		}
 	}
 
-	closesocket(s);
-	close(s);
-
-    return 0;
+	rudp_close(rudp_socket);
+	return 0;
 }
