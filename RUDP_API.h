ed15cc96160ca65fd7b0019abf929611adeca047
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-03-04 10:02:20 +0200

    Update RUDP_API.h

--- RUDP_API.h
+++ RUDP_API.h
@@ -3,7 +3,13 @@
 #include <stdbool.h>
 
 
-typedef struct _rudp_socket;
+// A struct that represents RUDP Socket
+typedef struct _rudp_socket{
+    int socket_fd; // UDP socket file descriptor
+    bool isServer; // True if the RUDP socket acts like a server, false for client.
+    bool isConnected; // True if there is an active connection, false otherwise.
+    struct sockaddr_in dest_addr; // Destination address. Client fills it when it connects via rudp_connect(), server fills it when it accepts a connection via rudp_accept().
+} RUDP_Socket;
 
 // Allocates a new structure for the RUDP socket (contains basic information about the socket itself).
 // Also creates a UDP socket as a baseline for the RUDP. isServer means that this socket acts like a server. 
